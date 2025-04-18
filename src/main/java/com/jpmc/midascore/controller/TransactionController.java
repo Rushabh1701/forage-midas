321f810b03df633d0ac6603c7c300d9809be656b
Rushabh1701 <rushabhhpatel@gmail.com>
2025-04-18 17:31:32 +0530

    Task5

---
+++ src/main/java/com/jpmc/midascore/controller/TransactionController.java
@@ -0,0 +1,18 @@
+package com.jpmc.midascore.controller;
+
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam Long userId) {
+        return transactionService.getBalance(userId);
+    }
+}
--- src/main/java/com/jpmc/midascore/service/TransactionService.java
+++ src/main/java/com/jpmc/midascore/service/TransactionService.java
@@ -2,6 +2,7 @@ package com.jpmc.midascore.service;
 
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
 import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.TransactionRecordRepository;
@@ -10,6 +11,8 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
 @Service
 public class TransactionService {
 
@@ -17,8 +20,9 @@ public class TransactionService {
     private final TransactionRecordRepository transactionRecordRepository;
     private final RestTemplate restTemplate;
 
-    // Constructor injection
-    public TransactionService(UserRepository userRepository, TransactionRecordRepository transactionRecordRepository, RestTemplate restTemplate) {
+    public TransactionService(UserRepository userRepository,
+                              TransactionRecordRepository transactionRecordRepository,
+                              RestTemplate restTemplate) {
         this.userRepository = userRepository;
         this.transactionRecordRepository = transactionRecordRepository;
         this.restTemplate = restTemplate;
@@ -26,49 +30,38 @@ public class TransactionService {
 
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "transaction-group")
     public void processTransaction(Transaction transaction) {
-
-        // Fetch sender and recipient from the repository by ID
         UserRecord sender = userRepository.findById(transaction.getSenderId()).orElse(null);
         UserRecord recipient = userRepository.findById(transaction.getRecipientId()).orElse(null);
 
-        //System.out.println("Users from service file: " + userRepository.findAll());
-        //System.out.println("Transaction SENDER_ID Works: " + transaction.getSenderId());
-        //System.out.println("Transaction sender id Works: " + transaction.getSenderId());
-        //System.out.println("Sender object: " + sender);
-
         if (sender != null && recipient != null && sender.getBalance() >= transaction.getAmount()) {
-
-            // If transaction is valid, record it
             TransactionRecord transactionRecord = new TransactionRecord(sender, recipient, transaction.getAmount());
             transactionRecordRepository.save(transactionRecord);
 
-            // Update balances of sender and recipient
             sender.setBalance(sender.getBalance() - transaction.getAmount());
+
             float incentiveAmount = 0;
-            try{
+            try {
                 incentiveAmount = getIncentiveApi(transaction).getAmount();
+            } catch (Exception e) {
+                System.out.println("Error occurred while calling Incentive API: " + e);
             }
-            catch
-            (Exception e) {
-                System.out.println("Error occurred while calling Incentive Api " + e);
-            }
-            // Update balance of recipient
+
             recipient.setBalance(recipient.getBalance() + transaction.getAmount() + Math.max(0, incentiveAmount));
 
-            // Persist updated users with their new balances
             userRepository.save(sender);
             userRepository.save(recipient);
         } else {
-            // If not valid, discard the transaction (no changes to database)
             System.out.println("Transaction discarded: " + transaction);
         }
-
-
     }
 
-    public Incentive getIncentiveApi(Transaction transaction){
+    public Incentive getIncentiveApi(Transaction transaction) {
         String url = "http://localhost:8080/incentive";
-        return restTemplate.postForObject(url, transaction, Incentive.class );
+        return restTemplate.postForObject(url, transaction, Incentive.class);
     }
 
+    public Balance getBalance(Long userId) {
+        Optional<UserRecord> user = userRepository.findById(userId);
+        return new Balance(user.map(UserRecord::getBalance).orElse(0f));
+    }
 }
